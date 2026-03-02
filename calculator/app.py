38a9c3ea9c567b36d2c9f70e437ffe2866466a71
Braige <therealbraige1@gmail.com>
2026-03-02 16:00:09 -0500

    timetravel

--- calculator/app.py
+++ calculator/app.py
@@ -1,1 +1,7 @@
+# to get new code to origin I have to 
 
+
+# 1. run git status
+# 2. run git add . 
+# 3. run git commit -m "whatever your doing in present tense in the text"
+# 4. run git push (sends it to origiin)
